b3adc6d90dc275be6be365469d282c0be5e20db6
Laura Roganovic <laura.roganovic@googlemail.com>
2022-10-20 15:41:09 +0200

    added error message for loading errors and styling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,11 +7,16 @@ import {useEffect, useState} from "react";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://react-food-app-11fa1-default-rtdb.firebaseio.com/meals.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -27,13 +32,25 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message)
+    });
   }, []);
 
   if (isLoading) {
     return (
         <section className={classes.MealsLoading}>
           <p>Loading...</p>
+        </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+        <section className={classes.MealsError}>
+          <p>{httpError}</p>
         </section>
     )
   }
